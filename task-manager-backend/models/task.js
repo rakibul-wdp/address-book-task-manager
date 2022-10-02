6aa45c28718149888b4071a446015a94b12ce358
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-03 04:25:00 +0600

    create task scheme data model

---
+++ task-manager-backend/models/task.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { dateValidator, dateConverter } = require('../utils');
+
+const taskSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name required']
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ['PENDING', 'COMPLETED'],
+      message: '{VALUE} is not a valid status. Should be PENDING or COMPLETED'
+    },
+    default: 'PENDING'
+  },
+  dueDate: {
+    type: String,
+    required: [true, 'Due date required'],
+    validate: {
+      validator: function (v) {
+        return dateValidator(v) && (dateConverter(v) > Date.now());
+      },
+      message: props => `${props.value} is not a valid date, must be in dd-mm-yyyy format and later than today!`
+    },
+  }
+});
+
+const taskModel = mongoose.model("task", taskSchema);
+
+module.exports = taskModel;
